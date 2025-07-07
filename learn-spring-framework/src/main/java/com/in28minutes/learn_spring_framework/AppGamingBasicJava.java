c88c502d0d9fbe2af7b7621720701bfdca0ac32a
sanghyup <hyupsang.dev@gmail.com>
2025-07-07 22:16:43 +0900

    sec2-loosecoupling1

--- learn-spring-framework/src/main/java/com/in28minutes/learn_spring_framework/AppGamingBasicJava.java
+++ learn-spring-framework/src/main/java/com/in28minutes/learn_spring_framework/AppGamingBasicJava.java
@@ -7,9 +7,9 @@ import com.in28minutes.learn_spring_framework.game.SuperContraGame;
 public class AppGamingBasicJava {
 
   public static void main(String[] args) {
-//    var marioGame = new MarioGame();
-    var superContraGame = new SuperContraGame();
-    var gameRunner = new GameRunner(superContraGame);
+    var game = new MarioGame();
+//    var game = new SuperContraGame();
+    var gameRunner = new GameRunner(game);
     gameRunner.run();
   }
 }
--- learn-spring-framework/src/main/java/com/in28minutes/learn_spring_framework/game/GameRunner.java
+++ learn-spring-framework/src/main/java/com/in28minutes/learn_spring_framework/game/GameRunner.java
@@ -1,15 +1,19 @@
 package com.in28minutes.learn_spring_framework.game;
 
 public class GameRunner {
-  private SuperContraGame game; //this is tightly coupled code!
-  //we need to make changes in the GameRunner class to be able to run another game
 
-  public GameRunner(SuperContraGame game){
+  // private SuperContraGame game;
+  // this is tightly coupled code!
+  // we need to make changes in the GameRunner class to be able to run another game
+  // use interface like below instead
+  private GamingConsole game;
+
+  public GameRunner(GamingConsole game) {
     this.game = game;
   }
 
   public void run() {
-    System.out.println("Running game: "+ game);
+    System.out.println("Running game: " + game);
     game.up();
     game.down();
     game.left();
