0654e74fa842f566823db3c81375dc5ab179f423
sanghyup <hyupsang.dev@gmail.com>
2025-07-14 23:18:22 +0900

    sec2-step10

--- learn-spring-framework/src/main/java/com/in28minutes/learn_spring_framework/HelloWorldConfiguration.java
+++ learn-spring-framework/src/main/java/com/in28minutes/learn_spring_framework/HelloWorldConfiguration.java
@@ -3,6 +3,15 @@ package com.in28minutes.learn_spring_framework;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
+
+record Person(String name, int age, Address address) {
+
+};
+
+record Address(String firstLine, String city) {
+
+};
+
 @Configuration
 public class HelloWorldConfiguration {
   // Spring beans: things that are managed by Spring
@@ -12,4 +21,33 @@ public class HelloWorldConfiguration {
     return "Ranga";
   }
 
+  @Bean
+  public int age() {
+    return 15;
+  }
+
+  @Bean
+  public Person person() {
+    return new Person("Ravi", 20, new Address("Main Street", "Utrecht"));
+  }
+
+  @Bean
+  public Person person2MethodCall() {
+    return new Person(name(), age(), address());
+  }
+
+  @Bean
+  public Person person3Parameters(String name, int age, Address address3) {
+    return new Person(name, age, address3);
+  }
+
+  @Bean(name = "address2")
+  public Address address() {
+    return new Address("Baker Street", "London");
+  }
+
+  @Bean(name = "address3")
+  public Address address3() {
+    return new Address("Motinagar", "Hyderabad");
+  }
 }
