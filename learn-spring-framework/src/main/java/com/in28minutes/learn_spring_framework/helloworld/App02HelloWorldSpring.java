98e312e1fd413486874ae34c890aa262b30c3640
sanghyup <hyupsang.dev@gmail.com>
2025-07-22 15:45:49 +0900

    sec2_end

--- learn-spring-framework/src/main/java/com/in28minutes/learn_spring_framework/helloworld/App02HelloWorldSpring.java
+++ learn-spring-framework/src/main/java/com/in28minutes/learn_spring_framework/helloworld/App02HelloWorldSpring.java
@@ -20,6 +20,10 @@ public class App02HelloWorldSpring {
     System.out.println(context.getBean("person3Parameters"));
     System.out.println(context.getBean("address2"));
 
-    // System.out.println(context.getBean(Address.class));
+    System.out.println(context.getBean(Person.class));
+    System.out.println(context.getBean(Address.class));
+
+    System.out.println(context.getBean("person5Qualifier"));
+
   }
 }
--- learn-spring-framework/src/main/java/com/in28minutes/learn_spring_framework/helloworld/HelloWorldConfiguration.java
+++ learn-spring-framework/src/main/java/com/in28minutes/learn_spring_framework/helloworld/HelloWorldConfiguration.java
@@ -1,7 +1,9 @@
 package com.in28minutes.learn_spring_framework;
 
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Primary;
 
 
 record Person(String name, int age, Address address) {
@@ -41,12 +43,26 @@ public class HelloWorldConfiguration {
     return new Person(name, age, address3);
   }
 
+  @Bean
+  @Primary
+  public Person person4Parameters(String name, int age, Address address) {
+    return new Person(name, age, address);
+  }
+
+  @Bean
+  public Person person5Qualifier(String name, int age,
+      @Qualifier("address3qualifier") Address address) {
+    return new Person(name, age, address);
+  }
+
   @Bean(name = "address2")
+  @Primary // this will be used when there are multiple candidates
   public Address address() {
     return new Address("Baker Street", "London");
   }
 
   @Bean(name = "address3")
+  @Qualifier("address3qualifier")
   public Address address3() {
     return new Address("Motinagar", "Hyderabad");
   }
